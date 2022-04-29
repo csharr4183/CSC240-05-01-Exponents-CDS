c85126e9925b041004a87b6af6c456f273d93209
Chad Sharritt <CSHARR4183@my.sullivan.edu>
2022-04-29 12:10:58 -0400

    Square finished

--- Exponents/Form1.cs
+++ Exponents/Form1.cs
@@ -8,8 +8,8 @@ using System.Text;
 using System.Windows.Forms;
 
 /**************************************
- * Name:
- * Date:
+ * Name:        Chad D. Sharritt
+ * Date:        4/29/2022
  * Description: A program using user-created method
  *              for the math function Square and Cube
  * Assistance:
@@ -27,19 +27,27 @@ namespace Exponents
         private void xGoButton_Click(object sender, EventArgs e)
         {
             // STORAGE
+            int num;
 
             // INPUT
+            num = Convert.ToInt32(xInputTextBox.Text);
+
 
             // PROCESS
             /* calculate the square of the number and add it to the output - 
              * do this with a method call that allows you to pass the number
              * and then receive the answer */
+            xOutputLabel.Text = "Square is: " + Square(num).ToString();
+            
+
 
             /* calculate the cube of the number and add it to the output - 
              * do this by creating a methd as in the squaring example above */
+            xOutputLabel.Text += "\nCube is: " + num.ToString();
 
             // OUTPUT
             // set the output to visible
+            xOutputLabel.Visible = true;
 
             // turn the Go button off
         }
@@ -47,11 +55,16 @@ namespace Exponents
         public int Square(int num)
         {
             // we will create this code in class
+            return num * num;
+
         }
+
 
         public int Cube(int num)
         {
             // we will create this code in class
+            return 0;
+
         }
 
         private void xResetButton_Click(object sender, EventArgs e)
